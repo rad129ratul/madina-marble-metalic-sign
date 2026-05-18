efe98faa0601875b7e73970ccfa7cc7acf1c7236
rad129ratul <ratulrs29@gmail.com>
2026-05-18 20:47:07 +0600

    update BASE_URL for deployment

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = '';
+export const BASE_URL = 'https://qty-kinase-roster-motor.trycloudflare.com';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
